c5cc5077073bf64df62de09282a359141fc699c0
Simon JAILLET <sjaillet@gmail.com>
2017-01-04 15:30:31 +0100

    Parses alternative control structures as dead code.

--- spec/Suite/Parser.spec.php
+++ spec/Suite/Parser.spec.php
@@ -378,6 +378,18 @@ describe("Parser", function() {
 
         });
 
+        it("parses alternative control structures as dead code", function () {
+
+            $filename = 'spec/Fixture/Parser/AlternativeControlStructures';
+            $content = file_get_contents($filename . '.php');
+
+            $parsed = Parser::debug($content);
+            expect($parsed)->toBe(file_get_contents($filename . '.txt'));
+
+            $parsed = Parser::parse($content);
+            expect(Parser::unparse($parsed))->toBe($content);
+        });
+
     });
 
 });
